cd420c2c4076e12652ba373fb5bcebe1c2a3836a
dongbin <1349975620@qq.com>
2022-04-20 08:57:41 +0800

    即墨区自然卫士-

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,117 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<tpns_flutter_plugin/XgFlutterPlugin.h>)
+#import <tpns_flutter_plugin/XgFlutterPlugin.h>
+#else
+@import tpns_flutter_plugin;
+#endif
+
+#if __has_include(<amap_flutter_navi/AmapFlutterNaviPlugin.h>)
+#import <amap_flutter_navi/AmapFlutterNaviPlugin.h>
+#else
+@import amap_flutter_navi;
+#endif
+
+#if __has_include(<fijkplayer/FijkPlugin.h>)
+#import <fijkplayer/FijkPlugin.h>
+#else
+@import fijkplayer;
+#endif
+
+#if __has_include(<flutter_app_upgrade/FlutterAppUpgradePlugin.h>)
+#import <flutter_app_upgrade/FlutterAppUpgradePlugin.h>
+#else
+@import flutter_app_upgrade;
+#endif
+
+#if __has_include(<flutter_baidu_mapapi_base/FlutterBmfbasePlugin.h>)
+#import <flutter_baidu_mapapi_base/FlutterBmfbasePlugin.h>
+#else
+@import flutter_baidu_mapapi_base;
+#endif
+
+#if __has_include(<flutter_baidu_mapapi_map/FlutterBmfmapPlugin.h>)
+#import <flutter_baidu_mapapi_map/FlutterBmfmapPlugin.h>
+#else
+@import flutter_baidu_mapapi_map;
+#endif
+
+#if __has_include(<flutter_bmflocation/LocationFlutterPlugin.h>)
+#import <flutter_bmflocation/LocationFlutterPlugin.h>
+#else
+@import flutter_bmflocation;
+#endif
+
+#if __has_include(<fluttertoast/FluttertoastPlugin.h>)
+#import <fluttertoast/FluttertoastPlugin.h>
+#else
+@import fluttertoast;
+#endif
+
+#if __has_include(<image_picker/FLTImagePickerPlugin.h>)
+#import <image_picker/FLTImagePickerPlugin.h>
+#else
+@import image_picker;
+#endif
+
+#if __has_include(<multi_image_picker/MultiImagePickerPlugin.h>)
+#import <multi_image_picker/MultiImagePickerPlugin.h>
+#else
+@import multi_image_picker;
+#endif
+
+#if __has_include(<package_info/FLTPackageInfoPlugin.h>)
+#import <package_info/FLTPackageInfoPlugin.h>
+#else
+@import package_info;
+#endif
+
+#if __has_include(<path_provider/FLTPathProviderPlugin.h>)
+#import <path_provider/FLTPathProviderPlugin.h>
+#else
+@import path_provider;
+#endif
+
+#if __has_include(<permission_handler/PermissionHandlerPlugin.h>)
+#import <permission_handler/PermissionHandlerPlugin.h>
+#else
+@import permission_handler;
+#endif
+
+#if __has_include(<shared_preferences/FLTSharedPreferencesPlugin.h>)
+#import <shared_preferences/FLTSharedPreferencesPlugin.h>
+#else
+@import shared_preferences;
+#endif
+
+#if __has_include(<url_launcher/FLTURLLauncherPlugin.h>)
+#import <url_launcher/FLTURLLauncherPlugin.h>
+#else
+@import url_launcher;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [XgFlutterPlugin registerWithRegistrar:[registry registrarForPlugin:@"XgFlutterPlugin"]];
+  [AmapFlutterNaviPlugin registerWithRegistrar:[registry registrarForPlugin:@"AmapFlutterNaviPlugin"]];
+  [FijkPlugin registerWithRegistrar:[registry registrarForPlugin:@"FijkPlugin"]];
+  [FlutterAppUpgradePlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterAppUpgradePlugin"]];
+  [FlutterBmfbasePlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterBmfbasePlugin"]];
+  [FlutterBmfmapPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterBmfmapPlugin"]];
+  [LocationFlutterPlugin registerWithRegistrar:[registry registrarForPlugin:@"LocationFlutterPlugin"]];
+  [FluttertoastPlugin registerWithRegistrar:[registry registrarForPlugin:@"FluttertoastPlugin"]];
+  [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
+  [MultiImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"MultiImagePickerPlugin"]];
+  [FLTPackageInfoPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPackageInfoPlugin"]];
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+  [PermissionHandlerPlugin registerWithRegistrar:[registry registrarForPlugin:@"PermissionHandlerPlugin"]];
+  [FLTSharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharedPreferencesPlugin"]];
+  [FLTURLLauncherPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTURLLauncherPlugin"]];
+}
+
+@end
