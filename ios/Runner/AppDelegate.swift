2c9641cf185dee834749e06521f2a1dda23c3577
dongbin <1349975620@qq.com>
2022-04-21 14:10:01 +0800

    即墨区自然卫士-vpn NS 接入 备份

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -5,6 +5,7 @@ import Flutter
 @objc class AppDelegate: FlutterAppDelegate,VSGServiceDelegate {
     
     var service = VSGService();
+    @objc dynamic var status = VSGVPNStatus.invalid;
     
   override func application(
     _ application: UIApplication,
@@ -12,30 +13,27 @@ import Flutter
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     
-    service.addObserver(self, forKeyPath: "status", options: NSKeyValueObservingOptions(rawValue: NSKeyValueObservingOptions.new.rawValue | NSKeyValueObservingOptions.old.rawValue) , context: nil);
+    service.addObserver(self, forKeyPath: "status", options: [.new, .old] , context: nil);
+    
+        
     service = VSGService.init(address: "222.173.76.34", port: 443, delegate: self);
     service.auth(withParam: "admin20G", paramKey: VSGAuthPassWordkUserName);
     service.auth(withParam: "Hh123456@", paramKey: VSGAuthPassWordkPassword);
-    service.startAuth(with: VSGResourceType.ncResource, callBack: nil);
+    service.startAuth(with: .ncResource, callBack: nil);
     
     
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-    
-    override class func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        //未生效
-        print("VSG: \(String(describing: keyPath))")
+
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         
-        //观察vpn链接状态
-        if keyPath == "status" {
-            print("VSGVPN链接状态：\(change!)");
-        }
-        
+        print("SVG：111111111111");
     }
+    
     func vsgService(_ service: VSGService!, authResult result: VSGAuthResult, param: [AnyHashable : Any]!) {
            
            switch result {
-           case VSGAuthResult.VSGAUTH_SUCCESS:
+           case .VSGAUTH_SUCCESS:
                print("VSG：success")
            default:
                break;
@@ -43,7 +41,7 @@ import Flutter
         print("VSG认证等链接状态：\(result)");
        }
     func vsgService(_ service: VSGService!, logoutResult result: VSGAuthResult) {
-        if result != VSGAuthResult.VSGAUTH_SUCCESS {
+        if result != .VSGAUTH_SUCCESS {
             print("VSG注销失败!")
         }
     }
